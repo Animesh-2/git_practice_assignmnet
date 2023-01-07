e9a49b0796f3527fb47b70f623cfc12ccfb589f7
animesh singh <animeshsingrol@gmail.com>
2023-01-07 10:52:04 +0530

    palindrome

--- prime_number.js
+++ prime_number.js
@@ -1,12 +1,30 @@
+//problem 1------->
+
 let number = 3;
 
 let factor=0;
 for(let i=1; i<=3; i++){
 
-if(number%i==0){
+if(number%i==1){
  
  factor++;
   }
 }
  
 (factor==2) ? console.log("prime") : console.log("not prime");
+
+
+
+//problem 2------->
+
+let str="naman";
+
+for(i=str.length-1;i>=0;i--){
+	
+	if(str[i] != str){
+		console.log("palindrome");
+		}
+	else{
+	console.log("not Palindrome");
+		}
+	}
